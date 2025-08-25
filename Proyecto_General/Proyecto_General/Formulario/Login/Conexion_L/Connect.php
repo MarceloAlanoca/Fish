d23323d7534e09c659218b5360a469180d72081e
Thiago Loaiza <thiagolosizat123@gmail.com>
2025-08-25 10:21:31 -0300

    Subo cambios generales de la pagina web

---
+++ Proyecto_General/Proyecto_General/Formulario/Login/Conexion_L/Connect.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $servidor = "localhost";
+    $usuario = "root";
+    $clave = " ";
+    $BaseDeDatos = "cliente";
+
+    $conexion = mysqli_connect($servidor, $usuario, $clave, $BaseDeDatos);
+
+?>
---
+++ Proyecto_General/Proyecto_General/Formulario/Registrar/Conexion_R/Conexion.php
@@ -0,0 +1,15 @@
+<?php
+    $bs_servidor = "localhost";
+    $bs_usuario = "root";
+    $bs_clave = "";
+    $bs_nombre = "usuarios_fs";
+
+    try{
+        $conn = mysqli_connect($bs_servidor, $bs_usuario, $bs_pass,
+        $bs_nombre);
+    }
+
+    catch(mysqli_sql_exception){
+            echo "Actualmente la base de datos esta apagada o hay datos faltantes";
+    }
+?>
