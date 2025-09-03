9a0ba449afb46ea13ee8de7bdbbbcd795b7b2933
Thiago-Loaiza <thiagolosizat123@gmail.com>
2025-09-03 20:59:59 -0300

    Fusion del regitro de Alejandra con el registro anterior

--- Proyecto_General/Views/Registro.php
+++ Proyecto_General/Views/Registro.php
@@ -11,26 +11,59 @@
     <form action="../PHP/Registro.php" method="post" id="form">
         <h1>Registro</h1>
         <div class="form">
-            
-            <div class="Grupo">
-                <input type="text" name="nombre" id="nombre" required><span class="barra"></span>
-                <label for="nombre">Nombre</label>
-            </div>
+
 
             <div class="Grupo">
-                <input type="email" name="email" id="email" required><span class="barra"></span>
+                <input type="text" name="username" id="username" required>
+                <span class="barra"></span>
+                <label for="username">Usuario</label>
+            </div>
+
+
+            <div class="Grupo">
+                <input type="text" name="nombre" id="nombre" required>
+                <span class="barra"></span>
+                <label for="nombre">Nombre de visualizacion</label>
+            </div>
+
+
+            <div class="Grupo">
+                <input type="email" name="email" id="email" required>
+                <span class="barra"></span>
                 <label for="email">Correo electrónico</label>
             </div>
 
+
             <div class="Grupo">
-                <input type="password" name="contraseña" id="contraseña" required><span class="barra"></span>
-                <label for="contraseña">Contraseña</label>
+                <input type="password" name="password" id="password" required>
+                <span class="barra"></span>
+                <label for="password">Contraseña</label>
+                <div class="show-container">
+                    <input type="checkbox" id="showPassword" class="checkbox">
+                    <span class="ShowPass">Mostrar Contraseña</span>
+                </div>
             </div>
 
-        <button type="submit" id="subir" name="subir" class="Enviar">Enviar</button>
+
+            <div class="Grupo">
+                <input type="password" name="passwordConfirm" id="passwordConfirm" required>
+                <span class="barra"></span>
+                <label for="passwordConfirm">Confirmar Contraseña</label>
+                <div class="show-container">
+                    <input type="checkbox" id="showPasswordConfirm" class="checkbox">
+                    <span class="ShowPass">Mostrar Contraseña</span>
+                </div>
+            </div>
+
+
+            <div id="mensaje" style="color: rgb(226, 0, 0); margin-top: 10px;"></div>
+
+
+            <button type="submit" id="subir" name="subir" class="Enviar">Enviar</button>
         </div>
     </form>
 
-    <script src="Formulario.js" defer></script>
+
+    <script src="../JS/registro.js" defer></script>
 </body>
 </html>
